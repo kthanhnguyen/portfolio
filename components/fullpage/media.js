6877bd0b377ba46ea57955ce74b09bcf208b3317
Thanh Nguyen <nguyenkimthanh901@gmail.com>
2020-03-04 22:39:43 +0700

    add intro

--- components/fullpage/media.js
+++ components/fullpage/media.js
@@ -9,19 +9,17 @@ import Mouse from "../mouse/mouse";
 import Section from "../section/section";
 import Page from "../page/page";
 import "./fullpage.css";
+import Typist from "react-typist";
 
 export const Home = withNavigationContext(({ fullpage }) => {
   return (
     <Section wrapper={false} backgroundColor="#292c35">
       <Content
         main={
-          <Lettering
-            title="INDEX"
-            text={[
-              "This is a single full page fixed screen.",
-              "Use the button bellow to navigate to the next page"
-            ]}
-          />
+          <Typist className="hello">
+            <h1>Hello, my name is Nguyen Kim Thanh</h1>
+            Use the button bellow to navigate to the next page
+          </Typist>
         }
         action={
           <div className="button">
@@ -40,33 +38,33 @@ export const Home = withNavigationContext(({ fullpage }) => {
   );
 });
 
-export const Third = withNavigationContext(({ fullpage }) => {
-  return (
-    <Section wrapper={false} backgroundColor="#ff6f5e">
-      <Background src="https://caferati.me/images/series/bojack-0.png" />
-      <Content
-        main={
-          <Lettering
-            title="PAGE-THREE"
-            text={["This is a screen with preloaded background image."]}
-          />
-        }
-        action={
-          <div className="button">
-            <AwesomeButton
-              size="large"
-              onPress={() => {
-                fullpage.navigate("/page-two");
-              }}
-            >
-              Goto the prev page
-            </AwesomeButton>
-          </div>
-        }
-      />
-    </Section>
-  );
-});
+// export const Third = withNavigationContext(({ fullpage }) => {
+//   return (
+//     <Section wrapper={false} backgroundColor="#ff6f5e">
+//       <Background src="https://caferati.me/images/series/bojack-0.png" />
+//       <Content
+//         main={
+//           <Lettering
+//             title="PAGE-THREE"
+//             text={["This is a screen with preloaded background image."]}
+//           />
+//         }
+//         action={
+//           <div className="button">
+//             <AwesomeButton
+//               size="large"
+//               onPress={() => {
+//                 fullpage.navigate("/page-two");
+//               }}
+//             >
+//               Goto the prev page
+//             </AwesomeButton>
+//           </div>
+//         }
+//       />
+//     </Section>
+//   );
+// });
 
 export const media = [
   {
@@ -82,21 +80,39 @@ export const media = [
         <Section wrapper={false} backgroundColor="#4158b4">
           <Content
             main={
-              <Lettering
-                title="PAGE-TWO"
-                text={[
-                  "This is multiple section page, scroll down to view more content."
-                ]}
-              />
+              <div className="intro">
+                <h1>INTRODUCTION</h1>
+                <div className="intro__item">
+                  <span>Fullname:</span>Nguyen Kim Thanh
+                </div>
+                <div className="intro__item">
+                  <span>Phone:</span>079-871-1321
+                </div>
+                <div className="intro__item">
+                  <span>Email:</span>nguyenkimthanh901@gmail.com
+                </div>
+                <div className="intro__item">
+                  <span>Address:</span>96/1/7, Phan Dinh Phung street, District
+                  Phu Nhuan, Ho Chi Minh city.
+                </div>
+                <div className="intro__item">
+                  <span>Date of Birth:</span>September 10th, 1996
+                </div>
+              </div>
             }
             action={<Mouse />}
           />
         </Section>
         <Section backgroundColor="#617be3">
-          <Lettering
-            title="PAGE-SECTION"
-            text={["This is a continued page section."]}
-          />
+          <div>
+            <h1>EDUCATION</h1>
+            <p>2014-2017: University of Science Ho Chi Minh city.</p>
+          </div>
+          <div>
+            <h1>EXPERIENCE</h1>
+            <p>12/2017- 2/2018: Intership FrontEnd at GMO-Z.com RUNSYSTEM</p>
+            <p>5/2018- 2020: Front End Developer at Transcosmos VietNam </p>
+          </div>
         </Section>
       </Page>
     )
@@ -105,6 +121,42 @@ export const media = [
     slug: "page-three",
     preload: ["https://caferati.me/images/series/bojack-0.png"],
     className: "slide page-three",
-    children: <Third />
+    children: (
+      <Page>
+        <Section backgroundColor="#2a2a2a">
+          <div>
+            <h1>PROJECT</h1>
+            <ul className="grid-list">
+              <li className="item">
+                <a className="screen">
+                  <div className="bar">
+                    <h2>SKY GO Desktop </h2>
+                    <i />
+                  </div>
+                  <div className="main">
+                    <div className="back">
+                      <img src="/static/monster.PNG" />
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <li className="item">
+                <a className="screen">
+                  <div className="bar">
+                    <h2>SKY GO Desktop </h2>
+                    <i />
+                  </div>
+                  <div className="main">
+                    <div className="back">
+                      <img src="/static/monster.PNG" />
+                    </div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Section>
+      </Page>
+    )
   }
 ];
--- pages/_app.js
+++ pages/_app.js
@@ -10,17 +10,12 @@ function App({ Component, pageProps }) {
   return (
     <Provider slug={router.route}>
       <Head>
-        <title>
-          Fullpage Javascript Navigation HOC with React Awesome Slider
-        </title>
+        <title>Portfolio Kim Thanh Nguyen</title>
         <meta
           name="viewport"
           content="width=device-width,minimum-scale=0.8,maximum-scale=1,user-scalable=no"
         />
-        <meta
-          name="description"
-          content="Fullpage navigation HOC with 3D animated transitions built on top of React Awesome Slider. GatsbyJS and NextJS recipes available."
-        />
+        <meta name="description" content="Portfolio Kim Thanh Nguyen" />
         <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <Layout>
